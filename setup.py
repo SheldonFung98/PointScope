aa84d6918934b67c549323542fa33f2a7498ca6f
SheldonVon <sheldonvonsr@gmail.com>
2023-07-11 22:54:34 +1000

    Ready for github install again

--- setup.py
+++ setup.py
@@ -1,18 +1,23 @@
 from setuptools import setup, find_packages
-import atexit
-import os
+from setuptools.command.install import install
+from setuptools.command.build_py import build_py
 
-def generate_protos():
-    from grpc_tools import _protoc_compiler
-    base_path = "pointscope"
-    grpc_gen_path = os.path.join(base_path, "protos")
-    _protoc_compiler.run_main([i.encode() for i in [
-        '', '-I.', '--python_out=.', '--pyi_out=.', '--grpc_python_out=.',
-        os.path.join(grpc_gen_path, "pointscope.proto")
-    ]])
 
-atexit.register(generate_protos)
+class CustomBuildCommand(build_py):
+    
+    def run(self):
+        self.generate_protos()
+        build_py.run(self)
 
+    def generate_protos(self):
+        from grpc_tools import _protoc_compiler
+        base_path = "pointscope"
+        grpc_gen_path = os.path.join(base_path, "protos")
+        _protoc_compiler.run_main([i.encode() for i in [
+            '', '-I.', '--python_out=.', '--grpc_python_out=.',
+            os.path.join(grpc_gen_path, "pointscope.proto")
+        ]])
+        
 setup(
     name='PointScope',
     python_requires=">=3",
@@ -27,4 +32,10 @@ setup(
         'vedo>=2023.4.4',
         'grpcio-tools'
     ],
+    setup_requires=[
+        'grpcio-tools'
+    ],
+    cmdclass={
+        'build_py': CustomBuildCommand,
+    }
 )
